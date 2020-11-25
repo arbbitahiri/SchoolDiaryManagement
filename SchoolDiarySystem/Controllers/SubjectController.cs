b88bdd48ff6e1e147fda8e2a9ec5d41328927532
Arb Tahiri <arb.tahiri@riinvest.net>
2020-11-25 16:33:41 +0100

    SubjectDAL, SubjectController(half), Subject Views

---
+++ SchoolDiarySystem/Controllers/SubjectController.cs
@@ -0,0 +1,107 @@
+﻿using SchoolDiarySystem.DAL;
+using SchoolDiarySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolDiarySystem.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private readonly SubjectsDAL subjectsDAL = new SubjectsDAL();
+
+        // GET: Subject
+        public ActionResult Index()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                var students = subjectsDAL.GetAll();
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = subjectsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = subjectsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = subjectsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
--- SchoolDiarySystem/DAL/SubjectsDAL.cs
+++ SchoolDiarySystem/DAL/SubjectsDAL.cs
@@ -1,7 +1,9 @@
-﻿using SchoolDiarySystem.Interface;
+﻿using SchoolDiarySystem.DAL.DataBase;
+using SchoolDiarySystem.Interface;
 using SchoolDiarySystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SchoolDiarySystem.DAL
@@ -10,32 +12,202 @@ namespace SchoolDiarySystem.DAL
     {
         public bool Create(Subjects model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Subject_Create";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "subjecttitle", model.SubjectTitle);
+                        DataConnection.AddParameter(command, "book", model.Book);
+                        DataConnection.AddParameter(command, "bookauthor", model.BookAuthor);
+                        DataConnection.AddParameter(command, "insertby", model.InsertBy);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+                        DataConnection.AddParameter(command, "teacherID", model.TeacherID);
+
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Update(Subjects model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Subject_Update";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "subjectID", model.SubjectID);
+                        DataConnection.AddParameter(command, "book", model.Book);
+                        DataConnection.AddParameter(command, "bookauthor", model.BookAuthor);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+                        DataConnection.AddParameter(command, "teacherID", model.TeacherID);
+
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Subject_Delete";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "subjectID", id);
+
+                        int result = command.ExecuteNonQuery();
+
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public Subjects Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Subjects subject = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Subject_Get";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "@subjectID", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            subject = new Subjects();
+                            while (reader.Read())
+                            {
+                                subject = ToObject(reader);
+                                if (reader["First_Name"] != DBNull.Value && reader["Last_Name"] != DBNull.Value)
+                                {
+                                    subject.Teacher = new Teachers
+                                    {
+                                        FirstName = reader["First_Name"].ToString(),
+                                        LastName = reader["Last_Name"].ToString()
+                                    };
+                                }
+                            }
+                        }
+                    }
+                }
+                return subject;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public List<Subjects> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Subjects> MySubjects = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Subject_ViewAll";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            MySubjects = new List<Subjects>();
+                            while (reader.Read())
+                            {
+                                var subject = ToObject(reader);
+                                if (reader["First_Name"] != DBNull.Value && reader["Last_Name"] != DBNull.Value)
+                                {
+                                    subject.Teacher = new Teachers
+                                    {
+                                        FirstName = reader["First_Name"].ToString(),
+                                        LastName = reader["Last_Name"].ToString()
+                                    };
+                                }
+                                MySubjects.Add(subject);
+                            }
+                        }
+                    }
+                }
+                return MySubjects;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Subjects ToObject(SqlDataReader dataReader)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var subject = new Subjects();
+
+                if (dataReader["SubjectID"] != DBNull.Value)
+                    subject.SubjectID = int.Parse(dataReader["SubjectID"].ToString());
+
+                if (dataReader["Subject_Title"] != DBNull.Value)
+                    subject.SubjectTitle = dataReader["Subject_Title"].ToString();
+
+                if (dataReader["Book"] != DBNull.Value)
+                    subject.Book = dataReader["Book"].ToString();
+
+                if (dataReader["Book_Author"] != DBNull.Value)
+                    subject.BookAuthor = dataReader["Book_Author"].ToString();
+
+                if (dataReader["InsertBy"] != DBNull.Value)
+                    subject.InsertBy = dataReader["InsertBy"].ToString();
+
+                if (dataReader["InsertDate"] != DBNull.Value)
+                    subject.InsertDate = DateTime.Parse(dataReader["InsertDate"].ToString());
+
+                if (dataReader["LUB"] != DBNull.Value)
+                    subject.LUB = dataReader["LUB"].ToString();
+
+                if (dataReader["LUD"] != DBNull.Value)
+                    subject.LUD = DateTime.Parse(dataReader["LUD"].ToString());
+
+                if (dataReader["LUN"] != DBNull.Value)
+                    subject.LUN = int.Parse(dataReader["LUN"].ToString());
+
+                if (dataReader["TeacherID"] != DBNull.Value)
+                    subject.TeacherID = int.Parse(dataReader["TeacherID"].ToString());
+
+                return subject;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
