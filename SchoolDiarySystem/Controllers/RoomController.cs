615a9d670193149256a080e00ccbcf84a2878b1f
Arb Tahiri <arb.tahiri@riinvest.net>
2020-11-25 15:07:08 +0100

    RoomsDAL, RoomController (half), Room Views

---
+++ SchoolDiarySystem/Controllers/RoomController.cs
@@ -0,0 +1,107 @@
+﻿using SchoolDiarySystem.DAL;
+using SchoolDiarySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolDiarySystem.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly RoomsDAL roomsDAL = new RoomsDAL();
+
+        // GET: Room
+        public ActionResult Index()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                var rooms = roomsDAL.GetAll();
+                return View(rooms);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = roomsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = roomsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = roomsDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+    }
+}
--- SchoolDiarySystem/DAL/RoomsDAL.cs
+++ SchoolDiarySystem/DAL/RoomsDAL.cs
@@ -1,7 +1,9 @@
-﻿using SchoolDiarySystem.Interface;
+﻿using SchoolDiarySystem.DAL.DataBase;
+using SchoolDiarySystem.Interface;
 using SchoolDiarySystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SchoolDiarySystem.DAL
@@ -10,32 +12,175 @@ namespace SchoolDiarySystem.DAL
     {
         public bool Create(Rooms model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Room_Create";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "roomNo", model.RoomNo);
+                        DataConnection.AddParameter(command, "roomType", model.RoomType);
+                        DataConnection.AddParameter(command, "insertby", model.InsertBy);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Update(Rooms model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Room_Update";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "roomID", model.RoomID);
+                        DataConnection.AddParameter(command, "roomno", model.RoomNo);
+                        DataConnection.AddParameter(command, "roomtype", model.RoomType);
+                        DataConnection.AddParameter(command, "insertby", model.InsertBy);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Room_Delete";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "roomID", id);
+
+                        int result = command.ExecuteNonQuery();
+
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public Rooms Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Rooms room = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Room_Get";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "roomID", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            room = new Rooms();
+                            while (reader.Read())
+                            {
+                                room = ToObject(reader);
+                            }
+                        }
+                    }
+                }
+                return room;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public List<Rooms> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Rooms> MyRooms = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Room_GetList";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            MyRooms = new List<Rooms>();
+                            while (reader.Read())
+                                MyRooms.Add(ToObject(reader));
+                        }
+                    }
+                }
+                return MyRooms;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Rooms ToObject(SqlDataReader dataReader)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var room = new Rooms();
+
+                if (dataReader["RoomID"] != DBNull.Value)
+                    room.RoomID = int.Parse(dataReader["RoomID"].ToString());
+
+                if (dataReader["Room_No"] != DBNull.Value)
+                    room.RoomNo = int.Parse(dataReader["Room_No"].ToString());
+
+                if (dataReader["Room_Type"] != DBNull.Value)
+                    room.RoomType = dataReader["Room_Type"].ToString();
+
+                if (dataReader["InsertBy"] != DBNull.Value)
+                    room.InsertBy = dataReader["InsertBy"].ToString();
+
+                if (dataReader["InsertDate"] != DBNull.Value)
+                    room.InsertDate = DateTime.Parse(dataReader["InsertDate"].ToString());
+
+                if (dataReader["LUB"] != DBNull.Value)
+                    room.LUB = dataReader["LUB"].ToString();
+
+                if (dataReader["LUD"] != DBNull.Value)
+                    room.LUD = DateTime.Parse(dataReader["LUD"].ToString());
+
+                if (dataReader["LUN"] != DBNull.Value)
+                    room.LUN = int.Parse(dataReader["LUN"].ToString());
+
+                return room;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
