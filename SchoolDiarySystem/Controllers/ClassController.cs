ad1ec3d7beadd5cf0a605b5875b726d994613f51
Arb Tahiri <arb.tahiri@riinvest.net>
2020-11-26 17:24:56 +0100

    ClassDAL, ClassController(half), Class Views

---
+++ SchoolDiarySystem/Controllers/ClassController.cs
@@ -0,0 +1,116 @@
+﻿using SchoolDiarySystem.DAL;
+using SchoolDiarySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolDiarySystem.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly ClassDAL classDAL = new ClassDAL();
+        private readonly RoomsDAL roomsDAL = new RoomsDAL();
+        private readonly TeachersDAL teachersDAL = new TeachersDAL();
+
+        // GET: Class
+        public ActionResult Index()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                var classes = classDAL.GetAll();
+                return View(classes);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Create()
+        {
+            if (UserSession.GetUsers != null)
+            {
+                GetTeachersAndRoom();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = classDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = classDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (UserSession.GetUsers != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                }
+
+                var students = classDAL.Get((int)id);
+                if (students == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(students);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        private void GetTeachersAndRoom()
+        {
+            ViewBag.TeacherID = new SelectList(teachersDAL.GetAll(), "TeacherID", "FullName");
+            ViewBag.RoomID = new SelectList(roomsDAL.GetAll(), "RoomID", "RoomType");
+        }
+    }
+}
--- SchoolDiarySystem/DAL/ClassDAL.cs
+++ SchoolDiarySystem/DAL/ClassDAL.cs
@@ -1,7 +1,9 @@
-﻿using SchoolDiarySystem.Interface;
+﻿using SchoolDiarySystem.DAL.DataBase;
+using SchoolDiarySystem.Interface;
 using SchoolDiarySystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SchoolDiarySystem.DAL
@@ -10,32 +12,189 @@ namespace SchoolDiarySystem.DAL
     {
         public bool Create(Class model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Class_Create";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "teacherID", model.TeacherID);
+                        DataConnection.AddParameter(command, "classNo", model.ClassNo);
+                        DataConnection.AddParameter(command, "roomID", model.RoomID);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+                        DataConnection.AddParameter(command, "insertby", model.InsertBy);
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Update(Class model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Class_Update";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "classID", model.ClassID);
+                        DataConnection.AddParameter(command, "teacherID", model.TeacherID);
+                        DataConnection.AddParameter(command, "roomID", model.RoomID);
+                        DataConnection.AddParameter(command, "LUN", model.LUN);
+                        DataConnection.AddParameter(command, "LUB", model.LUB);
+
+                        int result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Class_Delete";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "classID", id);
+
+                        int result = command.ExecuteNonQuery();
+
+                        return result > 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public Class Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Class classes = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Class_Get";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        DataConnection.AddParameter(command, "classID", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            classes = new Class();
+                            while (reader.Read())
+                            {
+                                classes = ToObject(reader);
+                                if (reader["Room_Type"] != DBNull.Value && reader["First_Name"] != DBNull.Value && reader["Last_Name"] != DBNull.Value)
+                                {
+                                    classes.Room = new Rooms { RoomType = reader["Room_Type"].ToString() };
+                                    classes.Teacher = new Teachers { FirstName = reader["First_Name"].ToString(), LastName = reader["Last_Name"].ToString() };
+                                }
+                            }
+                        }
+                    }
+                }
+                return classes;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public List<Class> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Class> MyClasses = null;
+                using (var connection = DataConnection.GetConnection())
+                {
+                    string sqlproc = "dbo.usp_Class_GetList";
+                    using (var command = DataConnection.GetCommand(connection, sqlproc, CommandType.StoredProcedure))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            MyClasses = new List<Class>();
+                            while (reader.Read())
+                            {
+                                var classes = ToObject(reader);
+                                if (reader["Room_Type"] != DBNull.Value && reader["First_Name"] != DBNull.Value && reader["Last_Name"] != DBNull.Value)
+                                {
+                                    classes.Room = new Rooms { RoomType = reader["Room_Type"].ToString() };
+                                    classes.Teacher = new Teachers { FirstName = reader["First_Name"].ToString(), LastName = reader["Last_Name"].ToString() };
+                                }
+                                MyClasses.Add(classes);
+                            }
+                        }
+                    }
+                }
+                return MyClasses;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Class ToObject(SqlDataReader dataReader)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var classes = new Class();
+
+                if (dataReader["ClassID"] != DBNull.Value)
+                    classes.ClassID = int.Parse(dataReader["ClassID"].ToString());
+
+                if (dataReader["TeacherID"] != DBNull.Value)
+                    classes.TeacherID = int.Parse(dataReader["TeacherID"].ToString());
+
+                if (dataReader["Class_No"] != DBNull.Value)
+                    classes.ClassNo = int.Parse(dataReader["Class_No"].ToString());
+
+                if (dataReader["RoomID"] != DBNull.Value)
+                    classes.RoomID = int.Parse(dataReader["RoomID"].ToString());
+
+                if (dataReader["InsertBy"] != DBNull.Value)
+                    classes.InsertBy = dataReader["InsertBy"].ToString();
+
+                if (dataReader["InsertDate"] != DBNull.Value)
+                    classes.InsertDate = DateTime.Parse(dataReader["InsertDate"].ToString());
+
+                if (dataReader["LUB"] != DBNull.Value)
+                    classes.LUB = dataReader["LUB"].ToString();
+
+                if (dataReader["LUD"] != DBNull.Value)
+                    classes.LUD = DateTime.Parse(dataReader["LUD"].ToString());
+
+                if (dataReader["LUN"] != DBNull.Value)
+                    classes.LUN = int.Parse(dataReader["LUN"].ToString());
+
+                return classes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
